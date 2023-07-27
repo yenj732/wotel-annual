4c861869007b1fabde8e854075d1df3b0be2e3d0
yenj732 <139729789+yenj732@users.noreply.github.com>
2023-07-27 10:59:37 +0800

    Add files via upload

---
+++ customFunction.js
@@ -0,0 +1,40 @@
+var liffID = '2000189044-Q1ZYKkXn';
+
+async function main() {
+    await liff.init({
+        liffId: liffID
+    }).then(function () {
+        console.log('LIFF init');
+
+        // 這邊開始寫使用其他功能
+        // liff.sendMessages([{
+        //     type: 'text',
+        //     text: 'Hello world'
+        // }])
+
+        const btnMessage = document.getElementById('sendButton');
+        btnMessage.addEventListener('click', () => {
+            let message = "Wotel周年慶活動參與:" + document.querySelector("#account").value;
+            console.log(message)
+            liff.sendMessages([
+                {
+                    type: 'text',
+                    text: message
+                }
+
+            ]).then(res => liff.closeWindow())
+                .catch(error => console.log(error));
+        });
+
+    }).catch(function (error) {
+        console.log(error);
+    });
+
+}
+
+main();
+
+
+function openCheckWindow(){
+    document.querySelector("#checkWindow").classList.toggle("active");
+}
